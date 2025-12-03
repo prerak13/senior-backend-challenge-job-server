f5fff1c1641f18bf8af9db27843764340d9398d1
Colin Hunt <colin@viafoura.com>
2025-12-03 13:22:43 -0500

    deleting previous template stuff

--- java-spring-boot-service/{{ cookiecutter.project_slug }}/core/src/main/java/com/viafoura/{{ cookiecutter.package_name }}/core/common/constants/FitnessFunctionsConstants.java
+++
@@ -1,5 +0,0 @@
-package com.viafoura.{{ cookiecutter.package_name }}.core.common.constants;
-
-public interface FitnessFunctionsConstants {
-  String PACKAGE_TO_SCAN = "com.viafoura.{{ cookiecutter.package_name }}";
-}
